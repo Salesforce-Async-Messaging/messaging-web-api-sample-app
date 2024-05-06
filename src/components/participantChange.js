eae957f7f36f33d7409ffa529818b539bad0a9e5
sachin-hanumantha <sachin.hanumantha@salesforce.com>
2024-05-06 13:04:07 -0700

    add participant change timestamp to the ui text

--- src/components/participantChange.js
+++ src/components/participantChange.js
@@ -1,11 +1,14 @@
 import "./participantChange.css";
 import * as ConversationEntryUtil from "../helpers/conversationEntryUtil";
+import { util } from "../helpers/common";
 
 export default function ParticipantChange({conversationEntry}) {
     function generateParticipantChangeText() {
         const participantName = ConversationEntryUtil.getParticipantChangeEventPartcipantName(conversationEntry);
-        const participantJoined = ConversationEntryUtil.hasParticipantJoined(conversationEntry);
-        const participantChangeText = `${participantName} ${participantJoined ? `joined` : `left`}`;
+        const hasParticipantJoined = ConversationEntryUtil.hasParticipantJoined(conversationEntry);
+        const fullStyleFormattedDateTime = util.formatDateTime(conversationEntry.transcriptedTimestamp);
+        const formattedTime = fullStyleFormattedDateTime.split(",")[1].trim();
+        const participantChangeText = `${participantName} ${hasParticipantJoined ? `joined` : `left`} at ${formattedTime}`;
 
         return participantChangeText;
     }
